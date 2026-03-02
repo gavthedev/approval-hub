25b3eed8f3150f2476e17844fa7b194420434186
gavthedev <emailformiscs@gmail.com>
2026-03-02 17:24:20 +0100

    chore: create approvals/urls and approvals/views

--- backend/approvals/views.py
+++ backend/approvals/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import RequestSerializer
+from rest_framework.generics import ListCreateAPIView
+from .models import Request
 
-# Create your views here.
+class RequestListCreateView(ListCreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = RequestSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        return Request.objects.filter(company__slug=slug)
