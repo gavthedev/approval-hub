3953db86cb2b70cc77d1ac7728dc707de21d94be
gavthedev <emailformiscs@gmail.com>
2026-03-03 16:29:43 +0100

    chore: Approve endpoint + RequestApproveView create

--- backend/approvals/urls.py
+++ backend/approvals/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RequestListCreateView
+from .views import RequestListCreateView, ApproveRequestCreateView
 
 urlpatterns = [
     path("requests/", RequestListCreateView.as_view(), name="request-list-create"),
+    path("requests/<int:pk>/approve/", ApproveRequestCreateView.as_view(), name="approve-request-create"),
+
 ]
--- backend/approvals/views.py
+++ backend/approvals/views.py
@@ -1,8 +1,9 @@
 from .serializers import RequestSerializer, ApprovalSerializer
 from rest_framework.generics import CreateAPIView, ListCreateAPIView
-from .models import Request
+from .models import Request, Approval
 from rest_framework.permissions import IsAuthenticated
 from companies.permissions import IsCompanyMember, IsCompanyApprover
+from rest_framework.exceptions import ValidationError
 
 class RequestListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated, IsCompanyMember]
@@ -12,14 +13,19 @@ class RequestListCreateView(ListCreateAPIView):
         slug = self.kwargs["slug"]
         return Request.objects.filter(company__slug=slug)
 
-class ApproveRequestView(CreateAPIView):
+class ApproveRequestCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated, IsCompanyApprover]
     serializer_class = ApprovalSerializer
 
     def perform_create(self, serializer):
         request_id = self.kwargs["pk"]
         approval_request = Request.objects.get(id=request_id)
+
+        if Approval.objects.filter(request=approval_request).exists():
+            raise ValidationError("This request has already been reviewed.")
+
         serializer.save(
             approver=self.request.user,
             request=approval_request
         )
+        approval_request.transition_to(Request.Status.APPROVED)
