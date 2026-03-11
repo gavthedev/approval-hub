2668651acc865f94456ce6243535ae6f5228cebf
gavthedev <emailformiscs@gmail.com>
2026-03-11 22:32:51 +0100

    chore: styling

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -11,10 +11,10 @@ export default function Dashboard() {
     }, []);
 
     return (
-        <div>
-            <h1>My Companies</h1>
+        <div className="min-h-screen min-w-screen flex flex-col items-center justify-center">
+            <h1 className="m-4 text ">My Companies</h1>
             {companies.map((company) => (
-                <div key={company.id}>
+                <div key={company.id} className="border rounded m-1">
                     <a href={`/company/${company.slug}`}>{company.name}</a>
                 </div>
             ))}
