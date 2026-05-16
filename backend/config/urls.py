8a2d63e3c0bfb51d6e12021546fae4e79ec4ff1e
gavthedev <emailformiscs@gmail.com>
2026-05-16 10:56:06 +0200

    fix: block unverified users from logging in

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenRefreshView, )
-from users.urls import CustomTokenObtainPairView
+from users.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
