44618bdf033554bdc8ef68eff347db62a77d5c06
gavthedev <emailformiscs@gmail.com>
2026-08-05 23:29:32 +0200

    fix: card correction

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -7,14 +7,13 @@ function Card({className, ...props}: React.ComponentProps<'div'>) {
         <div
             data-slot="card"
             className={cn(
-                'bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm',
+                'bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-2 shadow-sm',
                 className,
             )}
             {...props}
         />
     )
 }
-
 
 function CardHeader({className, ...props}: React.ComponentProps<'div'>) {
     return (
@@ -66,7 +65,7 @@ function CardContent({className, ...props}: React.ComponentProps<'div'>) {
     return (
         <div
             data-slot="card-content"
-            className={cn('px-6', className)}
+            className={cn('px-6 pb-2', className)}
             {...props}
         />
     )
@@ -76,7 +75,7 @@ function CardFooter({className, ...props}: React.ComponentProps<'div'>) {
     return (
         <div
             data-slot="card-footer"
-            className={cn('flex items-center px-6 [.border-t]:pt-6', className)}
+            className={cn('flex items-center px-2 [.border-t]:pt-6', className)}
             {...props}
         />
     )
